64c4827f176172cea79327d2d75130fab9d1da5f
DichNay <needdolars@gmail.com>
2026-03-29 21:09:21 +0500

    download tilewind final

--- App.tsx
+++ App.tsx
@@ -2,8 +2,9 @@ import { type FC} from "react";
 
 export const App:FC = function App() {
   return(
-    <>
-    </>
+    <div className="bg-blue-500 text-white p-10 rounded-xl shadow-lg">
+  Tailwind v4 работает!
+</div>
   )
 }
 
--- index.tsx
+++ index.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { App } from './App.tsx'
-import "tailwindcss";
+import "./style.css"; // Импортируем наш CSS файл со стилями Tailwind
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
